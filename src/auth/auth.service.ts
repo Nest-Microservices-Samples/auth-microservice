8ee55620ae058a121c8716478564e0da8594cfb8
Nicolay <nicolaymorenoherrera@gmail.com>
2024-11-12 19:54:21 -0500

    chore: Refactor loginUser method in AuthService for improved readability and error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -27,7 +27,7 @@ export class AuthService extends PrismaClient implements OnModuleInit {
                 }
             });
 
-            if (user) {
+            if ( user ) {
                 throw new RpcException({
                     status: 400,
                     message: 'User already exists',
@@ -37,12 +37,56 @@ export class AuthService extends PrismaClient implements OnModuleInit {
             const newUser = await this.user.create({
                 data: {
                     email,
-                    password: bcrypt.hashSync( password, 10 ),
+                    password: bcrypt.hashSync(password, 10),
                     name,
                 }
             });
 
             const { password: __, ...rest } = newUser;
+
+            return {
+                user: rest,
+                token: 'ABC',
+            }
+
+        } catch (error) {
+            throw new RpcException({
+                status: 400,
+                message: error.message,
+            });
+        }
+    }
+
+
+    async loginUser(loginUserDto: LoginUserDto) {
+
+        const { email, password } = loginUserDto;
+
+        try {
+
+            const user = await this.user.findUnique({
+                where: {
+                    email,
+                }
+            });
+
+            if ( !user ) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'User/Password not valid',
+                })
+            }
+
+            const isPasswordValid = bcrypt.compareSync( password, user.password );
+
+            if ( !isPasswordValid ) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'User/Password not valid',
+                })
+            }
+
+            const { password: __, ...rest } = user;
 
             return {
                 user: rest,
